9fbb2592e21661289fb6494c46ecbe69f4ad5a7d
aoigj100a <aoigj100@gmail.com>
2021-01-24 17:09:11 +0800

    初步完成待重構

--- app.js
+++ app.js
@@ -3,22 +3,30 @@ const app = express()
 const port = 3000
 const bodyParser = require('body-parser')
 const exphbs = require('express-handlebars')
+
+const talkShit = require('./talkShit.js')
+
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
 
-app.get('/',(req, res)=>{
+app.get('/', (req, res) => {
     // res.send('講幹話要開始囉')  
     res.render('index')
 })
 //取得表單資料
-app.post('/',(req, res)=>{
-    console.log(`req.body.jobs:${req.body.jobs}`)
+app.post('/', (req, res) => {
+    //取得處理好的結果 並且傳送至index 印在畫面
+    let result = talkShit(req.body)
+    //保留使用者輸入的資料
+    const options = req.body
+    
+    res.render('index', { result, options })
 
 })
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`已經連線到http://localhost:${port}`)
 })
--- talkShit.js
+++ talkShit.js
@@ -1,11 +1,36 @@
-function talkShit() {
-    //準備內容
-    const task = {
-        engineer: ['加個按鈕', '加新功能', '切個版', '改一點 code'],
-        designer: ['畫一張圖', '改個 logo', '順便幫忙設計一下', '隨便換個設計'],
-        entrepreneur: ['週末加班', '要能賺錢', '想個 business model', '找 VC 募錢']
+//隨機產生index 並回傳結果
+function pickRandom(array){
+    const index = Math.floor(Math.random() * array.length)
+    return array[index]
+}
 
+function talkShit(options) {
+    let phrase = ['很簡單', '很容易', '很快', '很正常']
+    let collection = []
+    let result 
+
+    if(!options.jobs){
+        result = "不要旋轉我！選個職業來！"
+        return result
     }
 
-    const phrase = ['很簡單', '很容易', '很快', '很正常']
+    if(options.jobs ==="工程師"){
+        collection = ['加個按鈕', '加新功能', '切個版', '改一點 code']
+        collection = pickRandom(collection)
+
+    }else if(options.jobs ==="設計師"){
+        collection = ['畫一張圖', '改個 logo', '順便幫忙設計一下', '隨便換個設計']
+        collection = pickRandom(collection)
+
+    }else if(options.jobs ==="創業家"){
+        collection = ['週末加班', '要能賺錢', '想個 business model', '找 VC 募錢']
+        collection = pickRandom(collection)
+    }
+
+    phrase = pickRandom(phrase)
+    result = `身為一個${options.jobs}，${collection}，${phrase}吧！`
+    
+    return result
 }
+
+module.exports = talkShit
